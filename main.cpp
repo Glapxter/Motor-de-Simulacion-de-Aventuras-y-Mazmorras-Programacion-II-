114515570808563f69dfe5e9fb0942d26f8bb808
Glapxter <anthonyjbm31@gmail.com>
2026-06-06 19:58:34 -0600

    Main finalizado

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include "SimulationEngine.h"
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+int main() {
+    cout << "=== Hyrule Adventure Simulation Engine ===" << endl;
+    try {
+        SimulationEngine engine(
+            "data/regions.txt",
+            "data/objects.txt",
+            "data/enemies.txt",
+            "data/link.txt",
+            "output/log.txt",
+            150
+        );
+        engine.run();
+    } catch (const exception& e) {
+        cerr << "Fatal error: " << e.what() << endl;
+        return 1;
+    }
     return 0;
 }
